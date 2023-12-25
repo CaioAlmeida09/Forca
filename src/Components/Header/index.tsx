da779f04d1c170733112e2f6d7b59ae762925deb
CaioAlmeida09 <Caio09092001@gmail.com>
2023-12-25 16:35:56 -0300

    ajuste Header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export function Header() {
   const location = useLocation();
@@ -9,9 +10,15 @@ export function Header() {
       style={{ background: "linear-gradient(to bottom, #2D2D2D, black)" }}
     >
       <section className="flex gap-5">
-        <p className=" font-medium md:text-xl text-base">Nivel 1</p>
-        <p className=" font-medium md:text-xl text-base">Nivel 2</p>
-        <p className=" font-medium md:text-xl text-base">Nivel 3</p>
+        <Link to="/nivel1" className=" font-medium md:text-xl text-base">
+          Nivel 1
+        </Link>
+        <Link to="/nivel2" className=" font-medium md:text-xl text-base">
+          Nivel 2
+        </Link>
+        <Link to="/nivel3" className=" font-medium md:text-xl text-base">
+          Nivel 3
+        </Link>
       </section>
       <h1 className=" text-lg font-bold">Bem-Vindo {name}</h1>
     </header>
